ab29b3d3f734b870171b48380b91b9c6d8cc8754
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2014-10-18 11:11:11 -0500

    Fix typo

--- check_ast/check_print_and_pdb.py
+++ check_ast/check_print_and_pdb.py
@@ -6,12 +6,13 @@ import logging
 
 """
 This script check py file to get "print" or "pdb" sentence.
-You can change the sentence "print" by sys.stdout.write()
-More info here:
-http://stackoverflow.com/questions/5574702/how-to-print-to-stderr-in-python
-Fix changing " print 'spam' " by
-import sys
-sys.stdout.write('spam\n')
+If you need print: 
+  you can change the sentence "print" by sys.stdout.write()
+  More info here:
+    http://stackoverflow.com/questions/5574702/how-to-print-to-stderr-in-python
+  Fix changing " print 'spam' " by
+    import sys
+    sys.stdout.write('spam\n')
 """
 
 def check_custom_lint(dir_path):
