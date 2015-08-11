5b20b2d20e16b0a8bcfd28ce84a81134e82dc776
Moises Lopez <moylop260@vauxoo.com>
2015-08-10 23:19:54 -0500

    [REF] autopep8: Add test cases

--- autopep8_extended/test_case.py
+++ autopep8_extended/test_case.py
@@ -1,10 +1,25 @@
+class my_snake_case_class2:
+    pass
+
 class my_snake_case_class:
     pass
 
+my_snake_case_class2()
 my_snake_case_class()
+
 
 my_line_with_trailing_space = 'dummy' 
 
         #       overidentation comment
 
-my_snake_case_class()
+parser = my_snake_case_class2
+
+
+def method1(param=None):
+    pass
+
+method1(param=my_snake_case_class2)
+
+my_dict = {
+    'parser': my_snake_case_class,
+}
